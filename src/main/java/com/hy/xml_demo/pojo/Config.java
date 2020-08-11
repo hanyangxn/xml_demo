04c1287331a32196291fa864b2a01ca17d7bfd0b
hanyangxn <m18110024862@163.com>
2020-08-11 14:29:16 +0800

    @Scope 与 extends AbstractFactoryBean 重写 isSingleton方法 实现功能一样

--- src/main/java/com/hy/xml_demo/pojo/Config.java
+++ src/main/java/com/hy/xml_demo/pojo/Config.java
@@ -3,6 +3,7 @@ package com.hy.xml_demo.pojo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.Resource;
@@ -20,6 +21,8 @@ public class Config {
     Environment environment;
 
     @Bean(name = "person1")
+    //@Scope 与 extends AbstractFactoryBean 重写 isSingleton方法 实现功能一样
+    @Scope(value="prototype")
     public Person person(){
         Person person = new Person(environment.getProperty("my_name"),Integer.valueOf(environment.getProperty("my_age")));
         return person;
--- src/main/java/com/hy/xml_demo/pojo/PersonTest.java
+++ src/main/java/com/hy/xml_demo/pojo/PersonTest.java
@@ -3,13 +3,8 @@ package com.hy.xml_demo.pojo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import javax.annotation.Resource;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
